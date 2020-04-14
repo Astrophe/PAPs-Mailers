4eb79a356261da9763837bca926e3d045c22e46f
Astrophe <git@astrohpeenterprises.com>
2020-04-14 17:35:55 -0400

    2nd Commit - added Jar and basic test logic

---
+++ src/main/java/com/example/PAPsMember/PAPsMember.java
@@ -0,0 +1,9 @@
+package com.example.PAPsMember;
+
+public class PAPsMember {
+    public static void main(String[] args) {
+        System.out.println("Hello PAPs");
+        int x = 1;
+        if (x == 1) System.out.println("x is 1!");
+    }
+}
